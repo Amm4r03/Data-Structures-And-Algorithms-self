0253d682b769a704a137bab15a8da5b4064856c5
ammar <akidwai03@gmail.com>
2023-12-08 23:15:58 +0530

    created linkedList

--- Linked Lists/DeletionLL.c
+++ Linked Lists/DeletionLL.c
@@ -11,8 +11,29 @@ struct Node
 
 struct Node* head;
 
+void insertNode(int value)
+{
+    struct Node* newNode = (struct Node*) malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = head;
+    head = newNode;
+}
+
+void printList()
+{
+    struct Node* traverse;
+    traverse = head;
+
+    while (traverse != NULL)
+    {
+        print("%d ", traverse->data);
+        traverse = traverse->next;
+    }
+    printf("\n");
+}
+
 int main()
 {
-
+    head = NULL;
     return 0;
 }
