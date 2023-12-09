3cff7f3bbb4c073b35be69ba06abdcbbeaa42fda
ammar <akidwai03@gmail.com>
2023-12-09 12:45:12 +0530

    returns middle element of LL - initialised

---
+++ Linked Lists/middleElement-LL.c
@@ -0,0 +1,23 @@
+// returns the middle element of a linked list
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node* next;
+};
+
+struct Node* head;
+
+void insertNode(int value)
+{
+    
+}
+
+int main()
+{
+    head = NULL;
+    return 0;
+}
