e048c04efc46271be6b9a7bb8bf9c6e225f1de10
ammar <akidwai03@gmail.com>
2023-12-10 21:59:12 +0530

    WIP

--- Linked Lists/merge2Sorted-LL.c
+++ Linked Lists/merge2Sorted-LL.c
@@ -1,1 +1,36 @@
 // merges two sorted linked lists
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node* next;
+};
+
+struct Node* head;
+
+void insertNode(int value)
+{
+
+}
+
+void printList()
+{
+
+}
+
+void mergeSorted(struct Node* head1, struct Node* head2)
+{
+    
+}
+
+int main()
+{
+    head = NULL;
+
+
+
+    return 0;
+}
